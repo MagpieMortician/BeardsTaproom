7ffaca3268153cc003d45cca0baab2b317235c63
Erin Deuel <end.gaia@gmail.com>
2019-11-15 11:34:57 -0800

    create keg object

---
+++ src/components/Keg.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Keg(props) {
+    return (
+        <div>
+            <h3>{props.brand} - {props.name}</h3>
+            <p>${props.price} alcohol content: {props.alcoholCon}</p>
+            <p>Pints left: {props.pints}</p>
+            <hr />
+        </div>
+    );
+}
+
+Ticket.propTypes = {
+    brand: PropTypes.string.isRequired,
+    names: PropTypes.string.isRequired,
+    price: PropTypes.string.isRequired,
+    alcoholCon: PropTypes.string.isRequired,
+    pints: PropTypes.int.isRequired
+};
+
+export default Ticket;
